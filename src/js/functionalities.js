444dd54d93064a87508a36058b4d2892b192d708
snt94 <eduardo775445@gmail.com>
2025-06-02 12:29:04 -0300

    .

--- src/js/functionalities.js
+++
@@ -1,8 +0,0 @@
-const themeBtn = document.getElementById('themeToggleBtn');
-const body = document.body;
-
-themeBtn.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-    themeBtn.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
-});
